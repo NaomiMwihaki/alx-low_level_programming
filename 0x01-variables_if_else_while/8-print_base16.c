73d41cbfdc9a75e68929567800d34078387e0c2a
NaomiMwihaki <mwihakikangethe24@gmail>
2023-06-16 09:51:49 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio:.h>
 /**
  * main - a program that prints all the numbers of base
  * 16 in lowercase, followed by a new line.
  * Return: 0
  */
 int main(void)
-{       
-        int f;
-        char low;
+{
+	int f;
+	char low;
 
-        for (f = '0'; f <= '9'; f++)
-                putchar(f);
+	for (f = '0'; f <= '9'; f++)
+		putchar(f);
 	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
 	putchar('\n');
