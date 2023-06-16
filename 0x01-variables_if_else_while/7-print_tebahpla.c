4d01af58a846be5dd12d8ecf24f97f629cb44ce3
NaomiMwihaki <mwihakikangethe24@gmail>
2023-06-16 09:16:47 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints the lowercase alphabet
+ * in reverse, followed by a new line.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
