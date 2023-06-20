4da19acf71827675c8e087a39dc9ef6fb237765e
NaomiMwihaki <mwihakikangethe24@gmail>
2023-06-20 05:58:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet - Make alphabets
- * Return: void
- */
+*print_alphabet- print all alphabet in lowercase
+*/
 void print_alphabet(void)
 {
-	char ch;
+	char letter;
 
-	for (ch = 'a'; ch <= 'z'; ch++);
-	_putchar(ch);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
 	_putchar('\n');
 }
-
