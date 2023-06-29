34e0105c47dc1d22267147fb3d46398affbbff74
NaomiMwihaki <mwihakikangethe24@gmail>
2023-06-29 01:32:39 -0700

    concatating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat -  concatenates two strings
+ * @src - source of string
+ * @dest - destination of string
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int string_length, z;
+
+	string_length = 0;
+
+	while (dest[string_length] != '\0')
+	{
+		string_length++;
+	}
+	for (z = 0; src[z] != '\0'; z++, string_length++)
+	{
+		dest[string_length] = src[z];
+	}
+	dest[string_length] = '\0';
+	return (dest);
+}
+
